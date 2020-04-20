1b7b79eb5a8e22ad705f199eb6f98fe09b4ca6bb
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2020-04-20 18:20:14 +0300

    1. Чтение кода и базовые ошибки[#257328]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,26 +1,26 @@
 package ru.job4j.tracker;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
-    private int ids = 1;
+    private final Item[] requests = new Item[100];
+    private int id = 1;
     private int size = 0;
 
-    public Item add(Item item) {
-        item.setId(generateId());
-        items[size++] = item;
-        return item;
+    public Item add(Item request) {
+        request.setId(generateId());
+        requests[size++] = request;
+        return request;
     }
 
     private String generateId() {
-        return String.valueOf(ids++);
+        return String.valueOf(id++);
     }
 
     public Item findById(String id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId().equals(id)) {
-                rsl = item;
+            Item current = requests[index];
+            if (current.getId().equals(id)) {
+                rsl = current;
                 break;
             }
         }
