75ff25575860169626d42131cfb2403c80d73360
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2020-05-20 19:32:02 +0300

    9.2. Тесты вывода на консоль в StartUI[33585#257299]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,11 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -13,5 +18,74 @@ public class StartUITest {
         StubAction action = new StubAction();
         new StartUI().init(input, new Tracker(), new UserAction[] { action });
         assertThat(action.isCall(), is(true));
+    }
+
+    @Test
+    public void whenPrtMenu() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Menu.")
+                .add("0. Stub action")
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+
+    @Test
+    public void whenCheckOutput() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("fix bug");
+        tracker.add(item);
+        ShowAllAction act = new ShowAllAction();
+        act.execute(new StubInput(new String[] {}), tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Item0: \"" + item.getName() + "\" id(" + item.getId() + ")")
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+
+    @Test
+    public void whenFindByNameFound() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("fix bug");
+        tracker.add(item);
+        FindByNameAction act = new FindByNameAction();
+        act.execute(new StubInput(new String[] {"fix bug"}), tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Item0: \"" + item.getName() + "\" id(" + item.getId() + ")")
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+
+    @Test
+    public void whenFindByNameNotFound() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("fix bug");
+        tracker.add(item);
+        FindByNameAction act = new FindByNameAction();
+        act.execute(new StubInput(new String[] {"bug"}), tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("No items with name \"bug\" was found")
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
     }
 }
