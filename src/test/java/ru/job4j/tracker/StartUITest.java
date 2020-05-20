afd2fd43d8631cdcd0b950b7bb1faede8800195d
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2020-05-20 19:03:52 +0300

    9. Написать тесты на StartUI.[#257312]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -14,29 +14,4 @@ public class StartUITest {
         new StartUI().init(input, new Tracker(), new UserAction[] { action });
         assertThat(action.isCall(), is(true));
     }
-
-    @Test
-    public void whenCallAction() {
-        Tracker t = new Tracker();
-        StartUI su = new StartUI();
-
-        StubInput choose0 = new StubInput(
-                new String[] {"0"}
-        );
-        StubAction action0 = new StubAction();
-        StubAction action1 = new StubAction();
-
-        su.init(choose0, t, new UserAction[] { action0, action1 });
-        assertThat(action0.isCall(), is(true));
-        assertThat(action1.isCall(), is(false));
-
-        StubInput choose1 = new StubInput(
-                new String[] {"1"}
-        );
-        action0 = new StubAction();
-        action1 = new StubAction();
-        su.init(choose1, t, new UserAction[] { action0, action1 });
-        assertThat(action0.isCall(), is(false));
-        assertThat(action1.isCall(), is(true));
-    }
 }
