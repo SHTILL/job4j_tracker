4c42734f03e0582e6b64d439f4c3426c7b10fcda
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2020-06-16 12:09:44 +0300

    2. Очередь с приоритетом на LinkedList[41670#257280]

--- src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -15,6 +15,7 @@ public class PriorityQueueTest {
         PriorityQueue queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
+        queue.put(new Task("a urgent", 1));
         queue.put(new Task("middle", 3));
         Task result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
