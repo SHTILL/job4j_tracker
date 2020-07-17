a67bd9a81a1dcb01af6d246d243eb8ed16228dd8
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2020-07-17 13:36:22 +0300

    1. Фильтрация учеников.[110058#257150]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,26 +10,37 @@ import java.util.stream.Collectors;
 import static org.junit.Assert.*;
 
 public class SchoolTest {
-    @Test
-    public void formClasses(){
-        List<Student> students = new ArrayList<>();
+    List<Student> students;
+
+    @Before public void initialize() {
+        students = new ArrayList<>();
         students.add(new Student("Petrov", 15));
         students.add(new Student("CleverGuy", 75));
         students.add(new Student("Zamyatin", 30));
         students.add(new Student("Lucky", 50));
         students.add(new Student("Unlucky", 49));
+    }
 
+    @Test
+    public void whenClassA() {
         List<Student> classA = School.collect(students, x -> x.getScore() >= 70);
-        System.out.println("class A:");
-        classA.stream().map(Student::getSurname).forEach(System.out::println);
+        List<Student> expected = List.of(new Student("CleverGuy", 75));
+        assertTrue(classA.containsAll(expected));
+    }
 
+    @Test
+    public void whenClassB() {
         List<Student> classB = School.collect(students, x -> x.getScore() >= 50 && x.getScore() < 70);
-        System.out.println("class B:");
-        classB.stream().map(Student::getSurname).forEach(System.out::println);
+        List<Student> expected = List.of(new Student("Lucky", 50));
+        assertTrue(classB.containsAll(expected));
+    }
 
+    @Test
+    public void whenClassV() {
         List<Student> classV = School.collect(students, x -> x.getScore() < 50);
-        System.out.println("class V:");
-        classV.stream().map(Student::getSurname).forEach(System.out::println);
-
+        List<Student> expected = List.of(   new Student("Petrov", 15),
+                                            new Student("Zamyatin", 30),
+                                            new Student("Unlucky", 49));
+        assertTrue(classV.containsAll(expected));
     }
 }
