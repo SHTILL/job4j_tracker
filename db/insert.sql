f46ed293d14cf3f2357253012254f79a11c2b58a
semen.slepakov <educationkvv@mail.ru>
2021-01-19 13:07:04 +0300

    1. Sql схема для проекта Tracker.[276987#335998]

---
+++ db/insert.sql
@@ -0,0 +1,4 @@
+insert into items(name) values ('Fix the bug');
+insert into items(name) values ('Buy a computer');
+insert into items(name) values ('Develop the feature');
+insert into items(name) values ('Print the file');
