f9eeb4f9ee90a6dd5ab39b472d34782a3447874d
semen.slepakov <educationkvv@mail.ru>
2021-01-22 15:02:56 +0300

    2. Трекер SQL[1734#335999]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -2,7 +2,6 @@ package ru.job4j.tracker;
 
 import java.io.InputStream;
 import java.sql.*;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
